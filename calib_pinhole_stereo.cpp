aaa7faa1aef2ce8d7e6bbc79ac04450bdee37cec
Zheng GONG <gongzheng.joey@gmail.com>
2017-07-07 22:14:24 +0800

    calib pinhole stereo done

--- calib_pinhole_stereo.cpp
+++ calib_pinhole_stereo.cpp
@@ -9,23 +9,27 @@
 using namespace std;
 using namespace cv;
 
-vector< vector< Point3f > > object_points;
+vector< vector< Point3f > > object_points,object_points1,object_points2;
 vector< vector< Point2f > > imagePoints1, imagePoints2;
 vector< Point2f > corners1, corners2;
 vector< vector< Point2f > > left_img_points, right_img_points;
 
 Mat img1, img2, gray1, gray2;
+Size im_size;
 
-void load_image_points(int board_width, int board_height, int num_imgs, float square_size,
-                      char* leftimg_dir, char* rightimg_dir, char* leftimg_filename, char* rightimg_filename) {
+void setup_calibration(int board_width, int board_height, int num_imgs,
+                       float square_size,
+                       char* leftimg_dir, char* rightimg_dir,
+                       char* leftimg_filename, char* rightimg_filename,
+                       char* extension) {
 
   Size board_size = Size(board_width, board_height);
   int board_n = board_width * board_height;
 
   for (int i = 1; i <= num_imgs; i++) {
     char left_img[100], right_img[100];
-    sprintf(left_img, "%s%s%d.jpg", leftimg_dir, leftimg_filename, i);
-    sprintf(right_img, "%s%s%d.jpg", rightimg_dir, rightimg_filename, i);
+    sprintf(left_img, "%s%s%d.jpg", leftimg_dir, leftimg_filename, i ,extension);
+    sprintf(right_img, "%s%s%d.jpg", rightimg_dir, rightimg_filename, i ,extension);
     img1 = imread(left_img, CV_LOAD_IMAGE_COLOR);
     img2 = imread(right_img, CV_LOAD_IMAGE_COLOR);
     cvtColor(img1, gray1, CV_BGR2GRAY);
@@ -55,7 +59,6 @@ void load_image_points(int board_width, int board_height, int num_imgs, float sq
     for (int i = 0; i < board_height; i++)
       for (int j = 0; j < board_width; j++)
         obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
-
     if (found1 && found2) {
       cout << i << ". Found corners!" << endl;
       imagePoints1.push_back(corners1);
@@ -73,7 +76,27 @@ void load_image_points(int board_width, int board_height, int num_imgs, float sq
     right_img_points.push_back(v2);
   }
 }
+double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints,
+                                 const vector< vector< Point2f > >& imagePoints,
+                                 const vector< Mat >& rvecs, const vector< Mat >& tvecs,
+                                 const Mat& cameraMatrix , const Mat& distCoeffs) {
+  vector< Point2f > imagePoints2;
+  int i, totalPoints = 0;
+  double totalErr = 0, err;
+  vector< float > perViewErrors;
+  perViewErrors.resize(objectPoints.size());
 
+  for (i = 0; i < (int)objectPoints.size(); ++i) {
+    projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+                  distCoeffs, imagePoints2);
+    err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+    int n = (int)objectPoints[i].size();
+    perViewErrors[i] = (float) std::sqrt(err*err/n);
+    totalErr += err*err;
+    totalPoints += n;
+  }
+  return std::sqrt(totalErr/totalPoints);
+}
 int main(int argc, char const *argv[])
 {
   char* leftcalib_file;
@@ -82,17 +105,22 @@ int main(int argc, char const *argv[])
   char* rightimg_dir;
   char* leftimg_filename;
   char* rightimg_filename;
+  char* extension;
   char* out_file;
   int num_imgs;
+  int board_width, board_height;
+  float square_size;
 
   static struct poptOption options[] = {
+    { "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
+    { "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
     { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
-    { "leftcalib_file",'u',POPT_ARG_STRING,&leftcalib_file,0,"Left camera calibration","STR" },
-    { "rightcalib_file",'v',POPT_ARG_STRING,&rightcalib_file,0,"Right camera calibration","STR" },
+    { "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Size of checkerboard square","NUM" },
     { "leftimg_dir",'L',POPT_ARG_STRING,&leftimg_dir,0,"Directory containing left images","STR" },
     { "rightimg_dir",'R',POPT_ARG_STRING,&rightimg_dir,0,"Directory containing right images","STR" },
     { "leftimg_filename",'l',POPT_ARG_STRING,&leftimg_filename,0,"Left image prefix","STR" },
     { "rightimg_filename",'r',POPT_ARG_STRING,&rightimg_filename,0,"Right image prefix","STR" },
+    { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
     { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
     POPT_AUTOHELP
     { NULL, 0, 0, NULL, 0, NULL, NULL }
@@ -102,24 +130,33 @@ int main(int argc, char const *argv[])
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
 
-  FileStorage fsl(leftcalib_file, FileStorage::READ);
-  FileStorage fsr(rightcalib_file, FileStorage::READ);
-
-  load_image_points(fsl["board_width"], fsl["board_height"], num_imgs, fsl["square_size"],
-                   leftimg_dir, rightimg_dir, leftimg_filename, rightimg_filename);
+  setup_calibration(board_width, board_height, num_imgs, square_size,
+                   leftimg_dir, rightimg_dir, leftimg_filename, rightimg_filename,extension);
 
   printf("Starting Calibration\n");
   Mat K1, K2, R, F, E;
   Vec3d T;
   Mat D1, D2;
-  fsl["K"] >> K1;
-  fsr["K"] >> K2;
-  fsl["D"] >> D1;
-  fsr["D"] >> D2;
+  vector< Mat > rvecs, tvecs;
+
+  int flag1 = 0;
+  flag1 |= CV_CALIB_FIX_K4;
+  flag1 |= CV_CALIB_FIX_K5;
+  calibrateCamera(object_points, imagePoints1, img1.size(), K1, D1, rvecs, tvecs, flag1);
+  cout << "Calibration 1 error: " <<
+          computeReprojectionErrors(object_points, imagePoints1, rvecs, tvecs, K1, D1) << endl;
+
+  rvecs.clear();
+  tvecs.clear();
+
+  calibrateCamera(object_points, imagePoints2, img2.size(), K2, D2, rvecs, tvecs, flag1);
+  cout << "Calibration 2 error: " <<
+          computeReprojectionErrors(object_points, imagePoints2, rvecs, tvecs, K2, D2) << endl;
+
   int flag = 0;
   flag |= CV_CALIB_FIX_INTRINSIC;
   
-  cout << "Read intrinsics" << endl;
+
   
   stereoCalibrate(object_points, left_img_points, right_img_points, K1, D1, K2, D2, img1.size(), R, T, E, F);
 
